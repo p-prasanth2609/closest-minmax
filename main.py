fbb13f83ee90dc0044b7699e3a63dc5d177cc1b2
Prasanth P <129147273+p-prasanth2609@users.noreply.github.com>
2023-07-14 21:31:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-A=list(map(int,input().split()))
+A = list(map(int, input().split()))
+
 min_value = float('inf')
 max_value = float('-inf')
 min_index = -1
 max_index = -1
+
 for i in range(len(A)):
     if A[i] < min_value:
         min_value = A[i]
@@ -10,5 +12,5 @@ for i in range(len(A)):
     if A[i] > max_value:
         max_value = A[i]
         max_index = i
-subarray_size = max_index - min_index + 1
+subarray_size = abs(max_index - min_index) + 1
 print(subarray_size)
