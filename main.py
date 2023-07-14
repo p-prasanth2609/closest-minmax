200232ea56fab0fa6e581dbd11653c68a5a5234e
satheeshkumar <psatheesh21120@gamil.com>
2023-07-14 20:57:37 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+A=list(map(int,input().split()))
+min_value = float('inf')
+max_value = float('-inf')
+min_index = -1
+max_index = -1
+for i in range(len(A)):
+    if A[i] < min_value:
+        min_value = A[i]
+        min_index = i
+    if A[i] > max_value:
+        max_value = A[i]
+        max_index = i
+subarray_size = max_index - min_index + 1
+print(subarray_size)
